75f787af5a6645962cae0b9a008687b8b4f5ddae
AJITH P. KUMAR <ajithpkumar231@gmail.com>
2022-05-31 05:45:31 +0530

    contact page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.scss'
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/Home'
-// import Home from './components/Home'
-// import About from './components/About'
+import About from './components/About'
+import Contact from './components/Contact'
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
